d70b34fda8c05baefa9b55a634dd0266fa873e20
Sebastian Radloff <sradloff23@gmail.com>
2014-10-11 17:30:02 -0500

    working on adding all chat info on load

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -18,6 +18,7 @@ $(document).ready(function() {
 	        $('.chat-messages-container').append('<div>' + message.content + '</div>');
 	        var chatBox = $('.chat-messages-container');
 	        chatBox.scrollTop(chatBox.prop("scrollHeight"));
+	        debugger;
       	}
     });
 
@@ -49,22 +50,22 @@ $(document).ready(function() {
 });
 
 function sendMessage() {
-    var message = $('.new-message').val();
+    // var message = $('.new-message').val();
 
-    var myFirebaseRef = new Firebase('https://scorching-torch-5624.firebaseio.com/');
-    // myFirebaseRef.remove();
-    myFirebaseRef.push({
-        content: message
-    });
+    // var myFirebaseRef = new Firebase('https://scorching-torch-5624.firebaseio.com/');
+    // // myFirebaseRef.remove();
+    // myFirebaseRef.push({
+    //     content: message
+    // });
 
 
-    myFirebaseRef.on('child_added', function(snapshot) {
-        var message = snapshot.val();
-        $('.new-message').val('');
-        $('.chat-messages-container').append('<div>' + message.content + '</div>');
-        var chatBox = $('.chat-messages-container');
-        chatBox.scrollTop(chatBox.prop("scrollHeight"));
-    });
+    // myFirebaseRef.on('child_added', function(snapshot) {
+    //     var message = snapshot.val();
+    //     $('.new-message').val('');
+    //     $('.chat-messages-container').append('<div>' + message.content + '</div>');
+    //     var chatBox = $('.chat-messages-container');
+    //     chatBox.scrollTop(chatBox.prop("scrollHeight"));
+    // });
 
     // var request = $.ajax({
     // 	url: '/messages',
