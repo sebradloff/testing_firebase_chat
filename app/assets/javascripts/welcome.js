46ad0057fc334389321361b114e2e97ead0a1a72
Sebastian Radloff <sradloff23@gmail.com>
2014-10-12 13:59:31 -0500

    fixed undefined issue

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
 
-		console.log((location.pathname).indexOf('chat_rooms') != -1);
-    if ((location.pathname).indexOf('chat_rooms') != -1) {
-    		
         var myFirebaseRef = new Firebase('https://scorching-torch-5624.firebaseio.com/');
         // automatically be at the bottom of the chat when you login to the page
         var chatBox = $('.chat-messages-container');
@@ -16,14 +13,8 @@ $(document).ready(function() {
         myFirebaseRef.once('value', function(snapshot) {
             // debugger;
             if (snapshot.val() == null) {
-                $('.chat-messages-container').append('<div>Be the first to comment!</div>');
-            } else {
-                var message = snapshot.val();
-                 $('.new-message').val('');
-                 $('.chat-messages-container').append('<div>' + message.content + '</div>');
-                 var chatBox = $('.chat-messages-container');
-                 chatBox.scrollTop(chatBox.prop("scrollHeight"));
-            }
+               $('.chat-messages-container').append('<div>Be the first to comment!</div>');
+            } 
         });
 
         var chatRoomID = location.pathname;
@@ -54,7 +45,6 @@ $(document).ready(function() {
 
             }
         });
-    }
 
 
 
