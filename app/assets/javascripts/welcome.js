59ebc4d561d04623dbd07ce69bd537de1fd61354
Sebastian Radloff <sradloff23@gmail.com>
2014-10-12 14:14:11 -0500

    got send button to work

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -6,12 +6,9 @@ $(document).ready(function() {
 
         chatBox.scrollTop(chatBox.prop("scrollHeight"));
 
-        $('.send-button').on('click', function() {
-            // sendMessage();
-        });
+        
 
         myFirebaseRef.once('value', function(snapshot) {
-            // debugger;
             if (snapshot.val() == null) {
                $('.chat-messages-container').append('<div>Be the first to comment!</div>');
             } 
@@ -29,11 +26,16 @@ $(document).ready(function() {
             }
         });
 
+        $('.send-button').on('click', function() {
+            var e = jQuery.Event("keypress");
+            e.which = 13;
+            $(document).trigger(e);
+        });
+
         $(document).keypress(function(event) {
 
             if (event.which == 13) {
                 var message = $('.new-message').val();
-
 
                 // myFirebaseRef.remove();
                 var chatRoomID = location.pathname;
@@ -46,41 +48,4 @@ $(document).ready(function() {
             }
         });
 
-
-
 });
-
-function sendMessage() {
-    // var message = $('.new-message').val();
-
-    // var myFirebaseRef = new Firebase('https://scorching-torch-5624.firebaseio.com/');
-    // // myFirebaseRef.remove();
-    // myFirebaseRef.push({
-    //     content: message
-    // });
-
-
-    // myFirebaseRef.on('child_added', function(snapshot) {
-    //     var message = snapshot.val();
-    //     $('.new-message').val('');
-    //     $('.chat-messages-container').append('<div>' + message.content + '</div>');
-    //     var chatBox = $('.chat-messages-container');
-    //     chatBox.scrollTop(chatBox.prop("scrollHeight"));
-    // });
-
-    // var request = $.ajax({
-    // 	url: '/messages',
-    // 	type: 'POST',
-    // 	data: {message: {content: message}},
-    // 	dataType: 'json'
-    // });
-
-    // request.done(function(response){
-    // 	$('.new-message').val('');
-    // 	$('.chat-messages-container').append('<div>'+response.content+'</div>');
-
-    // 	// scrolls to the bottom of the div to display the next message
-    // 	var chatBox = $('.chat-messages-container');
-    // 	chatBox.scrollTop(chatBox.prop("scrollHeight"));
-    // });
-}
